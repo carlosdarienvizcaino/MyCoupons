43db5f4e00e3287c32a2a8fc5e6e8b2d1c5d1746
oviouslee <lalva@ufl.edu>
2017-02-02 12:39:29 -0500

    updated google button
    Simply worked some magic and got the button rendering again.

--- client/app/account/account.routes.js
+++ client/app/account/account.routes.js
@@ -16,6 +16,7 @@ export default function routes($stateProvider) {
       controller: 'HomeController',
       controllerAs: 'vm'
     })
+
     .state('Help', {
       url: '/Help',
       template: require('./Help/Help.html'),
