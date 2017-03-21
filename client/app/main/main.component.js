aa1221544ab4c175822d318d46b62d9e7ca7e91c
oviouslee <lalva@ufl.edu>
2017-03-21 16:05:19 -0400

    fixed search bar and deleted a lot of useless code.

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -5,16 +5,14 @@ import routing from './main.routes';
 export class MainController {
 
   companies = [];
-  companyName = [];
-  companyDomain = [];
-  obj = [];
   couponsIds = [];
 
   /*@ngInject*/
-    constructor($rootScope, GoogleUser, GoogleUserResources, Coupons) {
+    constructor($scope, GoogleUser, GoogleUserResources, Coupons) {
       this.googleUser = GoogleUser;
       this.googleUserResources = GoogleUserResources;
       this.couponsService = Coupons;
+      $scope.company =""
     }
 
   $onInit(){
@@ -52,59 +50,21 @@ export class MainController {
 
 
   runsearch(company){
-    let counter = 0;
     if(this.googleUser.hasCredentials() ) {
       this.googleUserResources.queryMostRecentSearchedCoupon(this.googleUser, 5, company)
-       .then(res => {
-         this.coupons = res.data;
-         this.couponsId = res.data;
-         this.companies.length=0;
-         for( let i = 0 ; i < 5;i++) {
-           var id= this.coupons[i].id;
-           this.googleUserResources.queryforCompanyName(this.googleUser, id)
-             .then(res => {
-               this.companies.push(res.data);
-               counter++;
-               if (counter == 5) {
-                 this.companystringsplitter(this.companies, this.coupons);
-               }
-             })
-             .catch(error => {
-               console.log(error);
-             });
-         }
-       })
-       .catch(error => {
-       console.log(error);
-       });
-    }
-  }
+        .then(res => {
+          var ids = res.data;
+          this.couponsIds = [];
 
+          ids.map(obj => {
+            this.couponsIds.push(obj.id);
+          });
 
-  companystringsplitter(inputstring , coupons){
-  this.companyDomain.length=0;
-  this.companyName.length=0;
-
-   for(var i=0 ; i < inputstring.length ; i++)
-    {
-      var fields = inputstring[i].data.headers[0].value.split(/[<>]/);
-      this.companyName.push(fields[0]);
-      this.companyDomain.push(fields[1]);
-    }
-
-    this.outputobject(this.companyName, this.companyDomain, coupons);
-  }
-
-  outputobject (companyName, companyDomain, coupons){
-    this.obj.length=0;
-
-    for(var i = 0 ; i < coupons.length ; i++){
-      this.obj.push({
-        ID: coupons[i].id,
-        companyName: companyName[i],
-        companyDomain: companyDomain[i]
-      })
-
+          return this.couponsIds;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 
