c2f0c356d8c02846a66649c08e0f8defa4e388d4
carlosdarienvizcaino <carlos.vizcaino@ufl.edu>
2017-04-19 22:02:59 -0400

    Mongo endpoint for production

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/mycoupons'
+      || 'mongodb://Admin:Admin1@ds133249.mlab.com:33249/mycoupons'
   }
 };
